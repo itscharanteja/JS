e21b69510e8dc9184186abb5000703588aa505f8
Charan Teja <68527259+itscharanteja@users.noreply.github.com>
2023-04-24 16:50:23 +0530

    js work

--- match.js
+++ match.js
@@ -1,19 +1,19 @@
-// let twinkleStar = "Twinkle, twinkle, little star";
-// let starRegex = /Twinkle/gi; // Change this line
-// let a = twinkleStar.match(starRegex); // Change this line
-// console.log(a)
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/gi; // Change this line
+let a = twinkleStar.match(starRegex); // Change this line
+console.log(a)
 
-// let exampleStr = "Let's have fun with regular expressions!";
-// let unRegex = /.un/; // Change this line
-// let result = unRegex.test(exampleStr);
-// console.log(result)
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let result = unRegex.test(exampleStr);
+console.log(result)
 
-// let quoteSampl = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
-// let vowelRegex = /[aeiou]/gi; // Change this line
-// let result1 = quoteSampl.match(vowelRegex); // Change this line
-// console.log(result1)
+let quoteSampl = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; // Change this line
+let result1 = quoteSampl.match(vowelRegex); // Change this line
+console.log(result1)
 
 let quoteSample = "Blueberry 3.141592653s are delicious.";
-let myRegex = /[0-9a-z]/ig; // Change this line
-let result2 = quoteSample.match(myRegex); // Change this line
+let myRegex = /[h-s0-9]/gi; // Change this line
+let result2 = quoteSample.match(myRegex); // Change this line // Change this line
 console.log(result2)
