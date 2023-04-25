6f9fd1b061ea4fdcde37916a423454e644768671
Charan Teja <68527259+itscharanteja@users.noreply.github.com>
2023-04-25 21:59:01 +0530

    js work

--- match.js
+++ match.js
@@ -17,3 +17,8 @@ let quoteSample = "Blueberry 3.141592653s are delicious.";
 let myRegex = /[h-s0-9]/gi; // Change this line
 let result2 = quoteSample.match(myRegex); // Change this line // Change this line
 console.log(result2)
+
+let text = "<h1>Winter is coming</h1>";
+let myRegex1 = /<.*?>/; // Change this line
+let result4 = text.match(myRegex1);
+console.log(result4)
